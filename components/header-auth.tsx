3b111a52437e0a4ad19527f07589692fc244eea6
AvalonFX <fpapic2000@gmail.com>
2025-11-27 18:27:47 +0100

    Make Auth header dynamic for Supabase session

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -5,6 +5,8 @@ import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/server";
 
+export const dynamic = "force-dynamic";  // 👈 OVO DODAJ
+
 export default async function AuthButton() {
   const supabase = await createClient();
 
@@ -14,40 +16,37 @@ export default async function AuthButton() {
 
   if (!hasEnvVars) {
     return (
-      <>
-        <div className="flex gap-4 items-center">
-          <div>
-            <Badge
-              variant={"default"}
-              className="font-normal pointer-events-none"
-            >
-              Please update .env.local file with anon key and url
-            </Badge>
-          </div>
-          <div className="flex gap-2">
-            <Button
-              asChild
-              size="sm"
-              variant={"outline"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              size="sm"
-              variant={"default"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
-          </div>
+      <div className="flex gap-4 items-center">
+        <Badge
+          variant={"default"}
+          className="font-normal pointer-events-none"
+        >
+          Please update .env.local file with anon key and url
+        </Badge>
+        <div className="flex gap-2">
+          <Button
+            asChild
+            size="sm"
+            variant={"outline"}
+            disabled
+            className="opacity-75 cursor-none pointer-events-none"
+          >
+            <Link href="/sign-in">Sign in</Link>
+          </Button>
+          <Button
+            asChild
+            size="sm"
+            variant={"default"}
+            disabled
+            className="opacity-75 cursor-none pointer-events-none"
+          >
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
         </div>
-      </>
+      </div>
     );
   }
+
   return user ? (
     <div className="flex items-center gap-4">
       Hey, {user.user_metadata.username || user.email}!
